c3d955274539a9edd1269c81a28c131acf78a96e
Busiu <busiu69@gmail.com>
2019-03-25 11:07:56 +0100

    Examples are passing tests. Quality of code not as dope as it could be <sad_pepe>

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from parser2 import Parser2
 
 
 def main():
-    fh = open("example3.txt", "r")
+    fh = open("Tekscicho 3", "r")
     data = fh.read()
     my_parser = Parser2()
     result = my_parser.parse(data)
